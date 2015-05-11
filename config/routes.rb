14dc64fed062ec3ac06cb6a12d91f38a6a71c40d
Daniel Henrique <danielhmarinho@gmail.com>
2015-05-10 22:42:16 -0300

    Parser finish

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,13 @@
 Rails.application.routes.draw do
-  resources :clinicas do
-  collection {post :import}
-  end
+  resources :clinicas 
   resources :home
 
   root 'home#index'
 
   resources :drugs
-  resources :home
-  resources :parsers
+  resources :parsers do
+  collection {post :import}
+  end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
