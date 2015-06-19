c3d8841131b08339ac14c47bd993faf838adcdae
ricardogtx <ricardocdzg@hotmail.com>
2015-06-18 22:51:42 -0300

    adjusting clinical class

--- app/models/clinica.rb
+++ app/models/clinica.rb
@@ -1,4 +1,15 @@
 class Clinica < ActiveRecord::Base
   resourcify
 
+  def estado= nome_estado
+    self.state = State.find_by_name nome_estado
+  end
+
+  def estado
+    if self.state.nil?
+      nil
+    else
+      self.state.name
+    end
+  end
 end
